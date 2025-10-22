abd1e646281321aa24295dc6843ec543ac85c3cb
Bastien <usubelli@MacBook-Air-de-Bastien.local>
2025-10-22 15:21:30 +0200

    Ep1

---
+++ personne.ts
@@ -0,0 +1,16 @@
+interface Personne {
+  nom: string;
+  age: number;
+}
+
+function afficherPersonne(personne : Personne)
+{
+  console.log(`Bonjour, je m'appelle ${personne.nom} et j'ai ${personne.age} ans.`);
+}
+
+const personne1 : Personne = {
+  nom : "Alice",
+  age : 25
+}
+
+afficherPersonne(personne1);
