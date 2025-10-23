b9ba05f58b46d8fb60b758fe19488be155cd6b4f
BastienU <bastien.u04@gmail.com>
2025-10-23 09:35:24 +0200

    S2 Ep3

---
+++ S2/Ep3/generics.ts
@@ -0,0 +1,17 @@
+interface Voiture {
+  marque: string;
+  modele: string;
+  annee: number;
+}
+
+function getProperty<T, K extends keyof T>(obj: T, key: K): T[K] {
+  return obj[key];
+}
+
+const maVoiture: Voiture = {
+    marque: "Renault",
+    modele: "Scenic",
+    annee: 2014
+}
+
+console.log(getProperty(maVoiture, "marque"));
