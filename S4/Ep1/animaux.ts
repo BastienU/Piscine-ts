678e0b77f0ca403cff651432bbacef624ed96639
BastienU <bastien.u04@gmail.com>
2025-10-23 11:30:35 +0200

    S4 Ep1

---
+++ S4/Ep1/animaux.ts
@@ -0,0 +1,23 @@
+interface IAnimal
+{
+    nom: string;
+    seDeplacer():void
+}
+
+class Animal implements IAnimal
+{
+    nom: string;
+
+    constructor(nom: string)
+    {
+        this.nom = nom;
+    }
+
+    seDeplacer(): void {
+        console.log(`L'animal ${this.nom} se déplace.`);
+    }
+}
+
+const animal = new Animal("Patrick");
+animal.seDeplacer();
+
